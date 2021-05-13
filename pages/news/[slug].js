995424f5cf271c7496c108f56c854faa85acddde
William Li <email@williamli.dev>
2021-05-13 21:29:50 +0800

    fixed /news/[slug]

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import moment from 'moment'
 import { useRouter } from 'next/router'
 
